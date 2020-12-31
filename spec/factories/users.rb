9d2158034a0d9b5ebc1169e5126d8580427fe5c9
ko25252929 <76517744+ko25252929@users.noreply.github.com>
2020-12-31 23:31:18 +0900

    結合テストの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name {Faker::Name.last_name}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password {password }
+    password_confirmation {password }
   end
 end
